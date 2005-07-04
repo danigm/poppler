08cca4670f8ae65333beacda85834112e76489d1
Brad Hards <bradh@frogmouth.net>
2005-07-04 08:21:27 +0000

    Add in a simple demo/test application, similar to the pdffonts application in xpdf.

---
+++ qt4/tests/poppler-fonts.cpp
@@ -0,0 +1,76 @@
+#include <QtCore/QtCore>
+#include <iostream>
+
+#define UNSTABLE_POPPLER_QT4
+#include <poppler-qt4.h>
+
+int main( int argc, char **argv )
+{
+    QCoreApplication a( argc, argv );               // QApplication required!
+
+    if (!( argc == 2 ))
+    {
+	qWarning() << "usage: poppler-fonts filename";
+	exit(1);
+    }
+  
+    Poppler::Document *doc = Poppler::Document::load(argv[1]);
+    if (!doc)
+    {
+	qWarning() << "doc not loaded";
+	exit(1);
+    }
+
+    std::cout << "name                                 type         emb sub uni";
+    std::cout << std::endl;
+    std::cout << "------------------------------------ ------------ --- --- ---";
+    std::cout << std::endl;
+  
+    foreach( Poppler::FontInfo font, doc->fonts() ) {
+	std::cout << qPrintable( QString("%1").arg(font.name(), -37) );
+	switch( font.type() ) {
+	case Poppler::FontInfo::unknown:
+	    std::cout << "unknown      ";
+	    break;
+	case Poppler::FontInfo::Type1:
+	    std::cout << "Type 1       ";
+	    break;
+	case Poppler::FontInfo::Type1C:
+	    std::cout << "Type 1C      ";
+	    break;
+	case Poppler::FontInfo::Type3:
+	    std::cout << "Type 3       ";
+	    break;
+	case Poppler::FontInfo::TrueType:
+	    std::cout << "TrueType     ";
+	    break;
+	case Poppler::FontInfo::CIDType0:
+	    std::cout << "CID Type 0   ";
+	    break;
+	case Poppler::FontInfo::CIDType0C:
+	    std::cout << "CID Type 0C  ";
+	    break;
+	case Poppler::FontInfo::CIDTrueType:
+	    std::cout << "CID TrueType ";
+	    break;
+	}
+
+	if ( font.isEmbedded() ) {
+	    std::cout << "yes ";
+	} else {
+	    std::cout << "no  ";
+	}
+	if ( font.isSubset() ) {
+	    std::cout << "yes ";
+	} else {
+	    std::cout << "no  ";
+	}
+	if ( font.requiresUnicodeConversion() ) {
+	    std::cout << "yes ";
+	} else {
+	    std::cout << "no  ";
+	}
+
+	std::cout << std::endl;
+    }
+}
