e34713a2187e05358f98e749ec2923210a40a862
Kristian Høgsberg <krh@redhat.com>
2005-10-05 15:57:17 +0000

    2005-10-05  Kristian Høgsberg  <krh@redhat.com>
            * glib/poppler-page.cc (poppler_page_render_to_ps): Fix another
            off-by-one page number error (#4555).

--- glib/poppler-page.cc
+++ glib/poppler-page.cc
@@ -686,7 +686,7 @@ poppler_page_render_to_ps (PopplerPage   *page,
     ps_file->out = new PSOutputDev (ps_file->filename,
                                     ps_file->document->doc->getXRef(),
                                     ps_file->document->doc->getCatalog(),
-                                    ps_file->first_page + 1, ps_file->last_page,
+                                    ps_file->first_page, ps_file->last_page,
                                     psModePS, (int)ps_file->paper_width,
                                     (int)ps_file->paper_height, ps_file->duplex,
                                     0, 0, 0, 0, gFalse);
