769a89623c680f51690db1751522b016b02b83a8
Jeff Muizelaar <jeff@infidigm.net>
2006-12-20 19:55:55 +0000

    2006-12-20  Jeff Muizelaar  <jeff@infidigm.net>
    	* poppler/GlobalParams.cc: Try to make zero-width lines as close to
    	one pixel wide as we can. Fixes #9393.

--- poppler/CairoOutputDev.cc
+++ poppler/CairoOutputDev.cc
@@ -202,12 +202,18 @@ void CairoOutputDev::updateMiterLimit(GfxState *state) {
   cairo_set_miter_limit (cairo, state->getMiterLimit());
 }
 
+#define MIN(a,b) (a) < (b) ? (a) : (b)
+
 void CairoOutputDev::updateLineWidth(GfxState *state) {
   LOG(printf ("line width: %f\n", state->getLineWidth()));
   if (state->getLineWidth() == 0.0) {
-      cairo_set_line_width (cairo, 72.0/300.0);
+    /* find out how big pixels (device unit) are in the x and y directions
+     * choose the smaller of the two as our line width */
+    double x = 1.0, y = 1.0;
+    cairo_device_to_user_distance(cairo, &x, &y);
+    cairo_set_line_width (cairo, MIN(fabs(x),fabs(y)));
   } else {
-      cairo_set_line_width (cairo, state->getLineWidth());
+    cairo_set_line_width (cairo, state->getLineWidth());
   }
 }
 
