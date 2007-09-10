6eb159ef34eb7e2fb8adcfc48afd84d5f50efbbd
Albert Astals Cid <aacid@kde.org>
2007-09-10 17:38:05 +0000

    * autogen.sh: Support automake-1.10.         Patch by Krzysztof Kowalczyk <kkowalczyk@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -9,11 +9,12 @@ REQUIRED_AUTOMAKE_VERSION=${REQUIRED_AUTOMAKE_VERSION:-1.7}
 
 case $REQUIRED_AUTOMAKE_VERSION in
     1.4*) automake_progs="automake-1.4" ;;
-    1.5*) automake_progs="automake-1.5 automake-1.6 automake-1.7 automake-1.8 automake-1.9" ;;
-    1.6*) automake_progs="automake-1.6 automake-1.7 automake-1.8 automake-1.9" ;;
-    1.7*) automake_progs="automake-1.7 automake-1.8 automake-1.9" ;;
-    1.8*) automake_progs="automake-1.8 automake-1.9" ;;
-    1.9*) automake_progs="automake-1.9" ;;
+    1.5*) automake_progs="automake-1.5 automake-1.6 automake-1.7 automake-1.8 automake-1.9 automake-1.10" ;;
+    1.6*) automake_progs="automake-1.6 automake-1.7 automake-1.8 automake-1.9 automake-1.10" ;;
+    1.7*) automake_progs="automake-1.7 automake-1.8 automake-1.9 automake-1.10" ;;
+    1.8*) automake_progs="automake-1.8 automake-1.9 automake-1.10" ;;
+    1.9*) automake_progs="automake-1.9 automake-1.10" ;;
+    1.10*) automake_progs="automake-1.10" ;;
 esac
 
 # Print types
