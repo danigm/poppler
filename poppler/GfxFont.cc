0babb0eb1afa343fb12a3fd037b98d2d1c794a1c
Jonathan Kew <jonathan_kew@sil.org>
2008-01-28 20:42:44 +0100

    Introduce a check for strndup and alternative for systems without it

--- poppler/GfxFont.cc
+++ poppler/GfxFont.cc
@@ -974,7 +974,13 @@ static int parseCharName(char *charName, Unicode *uBuf, int uLen,
       return 0;	// .notdef or similar
     } else if (var_part != NULL) {
       // parse names of the form 7.oldstyle, P.swash, s.sc, etc.
+#ifdef HAVE_STRNDUP
       char *main_part = strndup(charName, var_part - charName);
+#else
+      char *main_part = (char*)gmalloc(var_part - charName + 1);
+      main_part[var_part - charName] = '\0';
+      memcpy(main_part, charName, var_part - charName);
+#endif
       GBool namesRecurse = gTrue, variantsRecurse = gFalse;
       int n = parseCharName(main_part, uBuf, uLen, namesRecurse, ligatures,
 			    numeric, hex, variantsRecurse);
