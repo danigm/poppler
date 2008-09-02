c75abac7ba07990ef54a46fa0d429eea580a71ef
Albert Astals Cid <aacid@kde.org>
2008-09-02 19:16:55 +0200

    Kjartan was missing and just agreed

--- utils/HtmlOutputDev.cc
+++ utils/HtmlOutputDev.cc
@@ -14,6 +14,9 @@
 //
 // Modified under the Poppler project - http://poppler.freedesktop.org
 //
+// All changes made under the Poppler project to this file are licensed
+// under GPL version 2 or later
+//
 // Copyright (C) 2005-2008 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2008 Kjartan Maraas <kmaraas@gnome.org>
 // Copyright (C) 2008 Boris Toloknov <tlknv@yandex.ru>
