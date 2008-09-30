d61e464754521555a06ed0901955f49be4def29a
Albert Astals Cid <aacid@kde.org>
2008-09-30 19:26:17 +0200

    Timothy also agreed, now Ed Catmur is the last one missing to answer

--- utils/ImageOutputDev.cc
+++ utils/ImageOutputDev.cc
@@ -10,6 +10,9 @@
 //
 // Modified under the Poppler project - http://poppler.freedesktop.org
 //
+// All changes made under the Poppler project to this file are licensed
+// under GPL version 2 or later
+//
 // Copyright (C) 2005, 2007 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2006 Rainer Keller <class321@gmx.de>
 // Copyright (C) 2008 Timothy Lee <timothy.lee@siriushk.com>
