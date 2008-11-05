82f4228a50efe6447c68f7f6d34a7805b7e4b75e
Albert Astals Cid <aacid@kde.org>
2008-11-05 20:57:17 +0100

    Add myself here

--- qt4/demos/viewer.cpp
+++ qt4/demos/viewer.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008, Pino Toscano <pino@kde.org>
+ * Copyright (C) 2008, Albert Astals Cid <aacid@kde.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
