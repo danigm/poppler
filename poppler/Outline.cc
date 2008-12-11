ab539f46089702f60b96b1ba2b2bc1544173c264
Albert Astals Cid <aacid@kde.org>
2008-12-11 23:15:45 +0100

    Fix crash when reading outline
    Fixes bug 19024. Last as reference is required if the item has children (Table 8.4) so if there is no last reference, don't try to read children.

--- poppler/Outline.cc
+++ poppler/Outline.cc
@@ -14,6 +14,7 @@
 // under GPL version 2 or later
 //
 // Copyright (C) 2005 Marco Pesenti Gritti <mpg@redhat.com>
+// Copyright (C) 2008 Albert Astals Cid <aacid@kde.org>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
@@ -131,6 +132,9 @@ GooList *OutlineItem::readItemList(Object *firstItemRef, Object *lastItemRef,
   OutlineItem *item;
   Object obj;
   Object *p;
+
+  if (!lastItemRef->isRef())
+    return NULL;
 
   items = new GooList();
   p = firstItemRef;
