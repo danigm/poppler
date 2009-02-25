f7c88148fdb671736d81dd5f01a3fb68f944510c
Koji Otani <sho@bbr.jp>
2009-02-25 23:38:35 +0100

    Fix cache shifting

--- poppler/GfxState.cc
+++ poppler/GfxState.cc
@@ -1705,7 +1705,7 @@ void GfxICCBasedCache::put(int numA, int genA,
   if (cache[GFX_ICCBASED_CACHE_SIZE-1].num > 0) {
     delete cache[GFX_ICCBASED_CACHE_SIZE-1].colorSpace;
   }
-  for (i = GFX_ICCBASED_CACHE_SIZE; i > 0; i--) {
+  for (i = GFX_ICCBASED_CACHE_SIZE-1; i > 0; i--) {
     if (cache[i - 1].num > 0) cache[i] = cache[i - 1];
   }
   cache[0].num = numA;
