cc4b61f19b69c31d9a73ae7361c4b6a94345d1c2
Albert Astals Cid <aacid@kde.org>
2009-03-08 12:55:45 +0100

    Fix the previous fix
    I should stop listening to people without checking that what they say is
    correct

--- poppler/DCTStream.cc
+++ poppler/DCTStream.cc
@@ -77,7 +77,6 @@ void DCTStream::init()
 {
   jpeg_std_error(&jerr);
   jerr.error_exit = &exitErrorHandler;
-  cinfo.err = &jerr;
   src.pub.init_source = str_init_source;
   src.pub.fill_input_buffer = str_fill_input_buffer;
   src.pub.skip_input_data = str_skip_input_data;
@@ -88,11 +87,12 @@ void DCTStream::init()
   src.str = str;
   src.index = 0;
   src.abort = false;
-  cinfo.src = (jpeg_source_mgr *)&src;
   current = NULL;
   limit = NULL;
   
   jpeg_create_decompress(&cinfo);
+  cinfo.err = &jerr;
+  cinfo.src = (jpeg_source_mgr *)&src;
   row_buffer = NULL;
 }
 
