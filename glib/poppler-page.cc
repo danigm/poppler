3210970dc1d6faf51bce59bb7ecb6b881f9c0fe6
Carlos Garcia Campos <carlosgc@gnome.org>
2009-04-10 13:09:37 +0200

    [glib] Print annotations with the print flag enabled

--- glib/poppler-page.cc
+++ glib/poppler-page.cc
@@ -522,6 +522,8 @@ poppler_page_set_selection_alpha (PopplerPage           *page,
 static GBool
 poppler_print_annot_cb (Annot *annot, void *user_data)
 {
+  if (annot->getFlags () & Annot::flagPrint)
+    return gTrue;
   return (annot->getType() == Annot::typeWidget);
 }
 
