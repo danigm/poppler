a87978b09a026b2db6b0f80271d87b44b336a121
Pino Toscano <pino@kde.org>
2009-06-03 13:49:56 +0200

    [Qt4 demo] show the checksum for embedded files

--- qt4/demos/embeddedfiles.cpp
+++ qt4/demos/embeddedfiles.cpp
@@ -58,7 +58,9 @@ void EmbeddedFilesDock::fillInfo()
         m_table->setItem(i, 2, new QTableWidgetItem(QString::number(file->size())));
         m_table->setItem(i, 3, new QTableWidgetItem(file->createDate().toString(Qt::SystemLocaleDate)));
         m_table->setItem(i, 4, new QTableWidgetItem(file->modDate().toString(Qt::SystemLocaleDate)));
-        m_table->setItem(i, 5, new QTableWidgetItem("n/a")); // ### FIXME pretty display of the HEX checksum
+        const QByteArray checksum = file->checksum();
+        const QString checksumString = !checksum.isEmpty() ? QString::fromAscii(checksum.toHex()) : QString::fromLatin1("n/a");
+        m_table->setItem(i, 5, new QTableWidgetItem(checksumString));
         ++i;
     }
 }
