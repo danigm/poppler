5b7b1cdc5c3b8652d3ae583b4d7b8b6de625adc2
Albert Astals Cid <aacid@kde.org>
2009-08-15 17:45:05 +0200

    Add an autotest showing previousresult fails

--- qt4/tests/check_search.cpp
+++ qt4/tests/check_search.cpp
@@ -7,6 +7,7 @@ class TestSearch: public QObject
     Q_OBJECT
 private slots:
     void bug7063();
+    void testNextAndPrevious();
 };
 
 void TestSearch::bug7063()
@@ -32,6 +33,56 @@ void TestSearch::bug7063()
     QCOMPARE( page->search(QString::fromUtf8("¥µ©"), pageRegion, Poppler::Page::FromTop, Poppler::Page::CaseSensitive), true );
     QCOMPARE( page->search(QString::fromUtf8("¥©"), pageRegion, Poppler::Page::FromTop, Poppler::Page::CaseSensitive), false );
   
+    delete doc;
+}
+
+void TestSearch::testNextAndPrevious()
+{
+    Poppler::Document *doc;
+    doc = Poppler::Document::load("../../../test/unittestcases/xr01.pdf");
+    QVERIFY( doc );
+  
+    Poppler::Page *page = doc->page(0);
+    QRectF region( QPointF(0,0), page->pageSize() );
+    
+    QCOMPARE( page->search(QString("is"), region, Poppler::Page::FromTop, Poppler::Page::CaseSensitive), true );
+    QVERIFY( qAbs(region.x() - 161.44) < 0.01 );
+    QVERIFY( qAbs(region.y() - 127.85) < 0.01 );
+    QVERIFY( qAbs(region.width() - 6.70) < 0.01 );
+    QVERIFY( qAbs(region.height() - 8.85) < 0.01 );
+    QCOMPARE( page->search(QString("is"), region, Poppler::Page::NextResult, Poppler::Page::CaseSensitive), true );
+    QVERIFY( qAbs(region.x() - 171.46) < 0.01 );
+    QVERIFY( qAbs(region.y() - 127.85) < 0.01 );
+    QVERIFY( qAbs(region.width() - 6.70) < 0.01 );
+    QVERIFY( qAbs(region.height() - 8.85) < 0.01 );
+    QCOMPARE( page->search(QString("is"), region, Poppler::Page::NextResult, Poppler::Page::CaseSensitive), true );
+    QVERIFY( qAbs(region.x() - 161.44) < 0.01 );
+    QVERIFY( qAbs(region.y() - 139.81) < 0.01 );
+    QVERIFY( qAbs(region.width() - 6.70) < 0.01 );
+    QVERIFY( qAbs(region.height() - 8.85) < 0.01 );
+    QCOMPARE( page->search(QString("is"), region, Poppler::Page::NextResult, Poppler::Page::CaseSensitive), true );
+    QVERIFY( qAbs(region.x() - 171.46) < 0.01 );
+    QVERIFY( qAbs(region.y() - 139.81) < 0.01 );
+    QVERIFY( qAbs(region.width() - 6.70) < 0.01 );
+    QVERIFY( qAbs(region.height() - 8.85) < 0.01 );
+    QCOMPARE( page->search(QString("is"), region, Poppler::Page::NextResult, Poppler::Page::CaseSensitive), false );
+    QCOMPARE( page->search(QString("is"), region, Poppler::Page::PreviousResult, Poppler::Page::CaseSensitive), true );
+    QVERIFY( qAbs(region.x() - 161.44) < 0.01 );
+    QVERIFY( qAbs(region.y() - 139.81) < 0.01 );
+    QVERIFY( qAbs(region.width() - 6.70) < 0.01 );
+    QVERIFY( qAbs(region.height() - 8.85) < 0.01 );
+    QCOMPARE( page->search(QString("is"), region, Poppler::Page::PreviousResult, Poppler::Page::CaseSensitive), true );
+    QVERIFY( qAbs(region.x() - 171.46) < 0.01 );
+    QVERIFY( qAbs(region.y() - 127.85) < 0.01 );
+    QVERIFY( qAbs(region.width() - 6.70) < 0.01 );
+    QVERIFY( qAbs(region.height() - 8.85) < 0.01 );
+    QCOMPARE( page->search(QString("is"), region, Poppler::Page::PreviousResult, Poppler::Page::CaseSensitive), true );
+    QVERIFY( qAbs(region.x() - 161.44) < 0.01 );
+    QVERIFY( qAbs(region.y() - 127.85) < 0.01 );
+    QVERIFY( qAbs(region.width() - 6.70) < 0.01 );
+    QVERIFY( qAbs(region.height() - 8.85) < 0.01 );
+    QCOMPARE( page->search(QString("is"), region, Poppler::Page::PreviousResult, Poppler::Page::CaseSensitive), false );
+    
     delete doc;
 }
 
