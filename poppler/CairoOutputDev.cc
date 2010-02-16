5edd175bdd0f7c2fc3aecb72384de4cb0788fc4e
Albert Astals Cid <aacid@kde.org>
2010-02-16 20:57:22 +0000

    Add and fix copyright years

--- poppler/CairoOutputDev.cc
+++ poppler/CairoOutputDev.cc
@@ -24,7 +24,7 @@
 // Copyright (C) 2008 Michael Vrable <mvrable@cs.ucsd.edu>
 // Copyright (C) 2008, 2009 Chris Wilson <chris@chris-wilson.co.uk>
 // Copyright (C) 2008 Hib Eris <hib@hiberis.nl>
-// Copyright (C) 2009 David Benjamin <davidben@mit.edu>
+// Copyright (C) 2009, 2010 David Benjamin <davidben@mit.edu>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
