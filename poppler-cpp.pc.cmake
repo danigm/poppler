aaa19a673020e12dbfceb4d25ade0ece7875fd83
Pino Toscano <pino@kde.org>
2010-05-04 12:07:43 +0200

    [cpp] make the pkg-config files really working

--- poppler-cpp.pc.cmake
+++ poppler-cpp.pc.cmake
@@ -6,4 +6,8 @@ includedir=${prefix}/include
 Name: poppler-cpp
 Description: cpp backend for Poppler PDF rendering library
 Version: @POPPLER_VERSION@
-Requires: poppler = @POPPLER_VERSION@
+Requires: @PC_REQUIRES@
+@PC_REQUIRES_PRIVATE@
+
+Libs: -L${libdir} -lpoppler-cpp
+Cflags: -I${includedir}/poppler/cpp
