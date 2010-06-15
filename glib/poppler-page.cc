e12b9ab105f3b56b47ded871693b939ed421c853
Carlos Garcia Campos <carlosgc@gnome.org>
2010-06-15 13:49:27 +0200

    [glib] Use poppler_image_mapping_free() instead of g_free()

--- glib/poppler-page.cc
+++ glib/poppler-page.cc
@@ -1140,7 +1140,7 @@ poppler_page_free_image_mapping (GList *list)
   if (list == NULL)
     return;
 
-  g_list_foreach (list, (GFunc)g_free, NULL);
+  g_list_foreach (list, (GFunc)poppler_image_mapping_free, NULL);
   g_list_free (list);
 }
 
