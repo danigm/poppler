ae517042570327b860c1db6b68f9697d5c104657
Pino Toscano <pino@kde.org>
2010-09-17 19:39:10 +0200

    [cpp] improve a bit the Unicode* -> ustring conversion
    (although IMHO not yet solved)

--- cpp/poppler-private.cpp
+++ cpp/poppler-private.cpp
@@ -84,11 +84,11 @@ ustring detail::unicode_GooString_to_ustring(GooString *str)
 
 ustring detail::unicode_to_ustring(const Unicode *u, int length)
 {
-    ustring str(length, 0);
-    ustring::iterator it = str.begin(), it_end = str.end();
+    ustring str(length * 2, 0);
+    ustring::iterator it = str.begin();
     const Unicode *uu = u;
-    for (; it != it_end; ++it) {
-        *it = ustring::value_type(*uu++);
+    for (int i = 0; i < length; ++i) {
+        *it++ = ustring::value_type(*uu++ & 0xffff);
     }
     return str;
 }
