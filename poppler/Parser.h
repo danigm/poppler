61445f5af749510fb647059af01abf1faf664195
Albert Astals Cid <aacid@kde.org>
2010-11-20 22:19:35 +0000

    forgot to upadte year here

--- poppler/Parser.h
+++ poppler/Parser.h
@@ -13,7 +13,7 @@
 // All changes made under the Poppler project to this file are licensed
 // under GPL version 2 or later
 //
-// Copyright (C) 2006 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2006, 2010 Albert Astals Cid <aacid@kde.org>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
